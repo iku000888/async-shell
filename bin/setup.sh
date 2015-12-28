8acf214e63f1a4fac0b845690f68120baeab030d
iku000888 <kanumaiku@gmail.com>
2015-12-29 03:02:17 +0900

    add a cheezy setup script.

---
+++ bin/setup.sh
@@ -0,0 +1,11 @@
+#Edit the values here so that it suts your needs.
+
+
+#Point to the port for the server side.
+echo "50008">~/.async_port
+
+#Setting up aliases for the commands
+#point to the client and server .py scripts
+#TODO:Specify location in a dynamic path manner.
+alias asy='python client.py'
+alias asy_init='python server.py'
